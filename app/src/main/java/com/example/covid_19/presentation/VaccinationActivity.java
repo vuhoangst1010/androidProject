d3669e227e39859f92edbffbfeee6d4b4253850f
du <dumuoisokhong@gmail.com>
2021-10-30 23:19:23 +0700

    Xdu_suaGraphicRegisterOke

--- app/src/main/java/com/example/covid_19/presentation/VaccinationActivity.java
+++ app/src/main/java/com/example/covid_19/presentation/VaccinationActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.covid_19.R;
 import com.example.covid_19.model.entity.User;
@@ -20,8 +23,16 @@ public class VaccinationActivity extends AppCompatActivity {
 
     public void register(View view) {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        User infor = new User("du","0369662341","12/10/2000","male","15856575","bac ninh");
+        DatabaseReference myRef = database.getReference("UserInfor");
+        String id = ((TextView)findViewById(R.id.idUser)).getText().toString();
+        String username = ((TextView)findViewById(R.id.username)).getText().toString();
+        String birthDate = ((TextView)findViewById(R.id.birthDate)).getText().toString();
+        String address = ((TextView)findViewById(R.id.address)).getText().toString();
+        String phoneNumber = ((TextView)findViewById(R.id.phoneNumber)).getText().toString();
+        RadioGroup rg = (RadioGroup) findViewById(R.id.gender);
+        String gender =((RadioButton)findViewById(rg.getCheckedRadioButtonId())).getText().toString();
+        User infor = new User(username,phoneNumber,birthDate,gender,id,address);
         myRef.child(infor.getId()).setValue(infor);
+        ((TextView)findViewById(R.id.status)).setText("đăng kí thành công");
     }
 }
